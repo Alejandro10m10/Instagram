57936cc208d05c5f0bda25bf2eac09a37a551da5
Alejandro Sánchez <alejandro10m_568@hotmail.com>
2022-02-28 12:30:39 -0600

    Adding the function to avoid instagram´s CORS policy and show the photos of a certain instagram profile

---
+++ src/js/corsDown.js
@@ -0,0 +1,27 @@
+/* 
+    Instagram does not return the image of the media, so Instagram returns the mistake: ERR_BLOCKED_BY_RESPONSE 
+    
+    Using the Google translation vulnerability, it can display any image URL, with or without permission. 
+    All these processes are done by the visitor's IP and computer.
+
+    With the following code we can avoid instagram´s CORS policy and show the photos of a certain profile
+
+*/
+"use strict";
+class cDg {
+    constructor(id) {
+        this.id = id;
+    }
+    view() {
+        var url = document.getElementById(this.id).src;
+        const p = url.split("/");
+        var t = '';
+        for (let i = 0; i < p.length; i++) {
+            if(i==2){
+                t += p[i].replaceAll('-', '--').replaceAll('.','-')+atob('LnRyYW5zbGF0ZS5nb29n')+'/';
+            } else { if(i != p.length-1){ t += p[i]+'/'; } else { t += p[i]; } }
+        }
+        document.getElementById(this.id).src = encodeURI(t);
+        return encodeURI(t);
+    }
+}
