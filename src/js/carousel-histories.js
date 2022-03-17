d087c30c417d6553fea12e917da23ab98e64761d
Alejandro Sánchez <alejandro10m_568@hotmail.com>
2022-03-17 11:13:43 -0600

    Add the function to go through the stories to the limit on the right

--- src/js/carousel-histories.js
+++ src/js/carousel-histories.js
@@ -7,14 +7,29 @@ let transaleX = 0;
 btnNextHistories.addEventListener('click', nextHistories);
 btnPreviusHistories.addEventListener('click', previusHistories);
 
+
 // Function that allow us to loop to the next block of histories that are not visible
 function nextHistories(){
     transaleX += (-320);
-    if(histories.length * 70 >= transaleX){
-        console.log('auqi');
+    /* Recorremos 4 historias con 320 pixeles a la izquierda, primero ya mostramos las primeras 4 y por eso la restamos
+       Por lo tanto hacemos una regla de tres para conocer cuantos pixeles necesitamos para recorrer todas las historias. */
+    allHistories = (histories.length * 320) / 4 - 320;
+
+    let saltos = histories.length / 8;
+    let missingStories = histories.length - (8 * Math.trunc(saltos));
+    let saltosRestantes = (8 * Math.trunc(saltos)) * 80;
+
+    if(transaleX - 320 == -saltosRestantes){
+        transaleX = -(80 * (8 + missingStories) + 15);
+        for(let historie of histories){
+            historie.style.transform = `translateX(${transaleX}px)`;
+        }
+        hideBtnNextHistories();
+        return;
     }
-    for(let historie of histories){
-        historie.style.transform = `translateX(${transaleX}px)`;
+
+    if(!(transaleX <= -(allHistories))){
+        for(let historie of histories) historie.style.transform = `translateX(${transaleX}px)`;
     }
 }
 
@@ -24,4 +39,8 @@ function previusHistories(){
     for(let historie of histories){
         historie.style.transform = `translateX(${transaleX}px)`;
     }
+}
+
+function hideBtnNextHistories(){
+    addClass(btnNextHistories, 'no-display');
 }
